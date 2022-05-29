7deff23b2f64b9218b272beb092d9a6e976110e5
chudaeyoun <chudaeyoun@tosspayments.com>
2022-05-29 17:20:11 +0900

    mysql, postgres 의존성 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,6 +19,8 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
     implementation("org.springframework.boot:spring-boot-starter-jdbc")
     runtimeOnly("com.h2database:h2")
+    implementation("mysql:mysql-connector-java")
+    implementation("org.postgresql:postgresql")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
